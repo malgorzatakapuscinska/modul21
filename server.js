5d8e93eebd66c7d4b2c95aa8b5c6bb8bf7e2ad67
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-05-03 21:29:33 +0200

    Install Express and change user and password

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log('Example app listening on port 3000!'));
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:strongPassword@ds261969.mlab.com:61969/nodeappdatabase');
+mongoose.connect('mongodb://gosia:matrix@ds111420.mlab.com:11420/nodeappdatabase');
 
 const userSchema = new Schema({
   name: String,
